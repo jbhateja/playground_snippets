15d89c90d8977b66bfc285f825ce1fd07c1a3f07
Jatin Bhateja <jatin.bhateja@gmail.com>
2024-05-03 07:51:11 +0530

    Records vs Values

---
+++ java/valhalla/Records.java
@@ -0,0 +1,27 @@
+
+// Identity objects.
+record ABC (long x, long y) {}
+
+public class Records {
+   public static void main(String [] args) {
+       ABC obj1 = new ABC(10, 20);
+       ABC obj2 = new ABC(10, 20);
+       
+       if (obj1 == obj2) {
+          System.out.println("Proved referencial equality == ");
+       } else if (obj1.equals(obj2)) {
+          System.out.println("Proved value equality .equals ");
+       } else {
+          System.out.println("Proved non-equality ");
+       }
+
+       synchronized (obj1) {
+          System.out.println("Record object as monitor ");
+          long res = 0;
+          for (int i = 0; i < 10000; i++) {
+              res += obj2.x() + obj2.y();
+          }
+          System.out.println("[obj2.x] " + obj2.x() + "  [obj2.y] " + obj2.y());
+       }
+   }
+}
---
+++ java/valhalla/Values.java
@@ -0,0 +1,44 @@
+
+
+// Value object.
+value class VABC {
+   long x;
+   long y;
+   VABC (long x, long y) {
+      this.x = x;
+      this.y = y;
+   }
+   long y() { return y; }
+   long x() { return x; }
+}
+
+public class Values {
+   public static void main(String [] args) {
+       VABC obj1 = new VABC(10, 20);
+       VABC obj2 = new VABC(10, 20);
+       if (obj1 == obj2) {
+          if (obj1.equals(obj2)) {
+             System.out.println("Proved referencial equality == which is same as .equals ");
+          }
+       } else if (obj1.equals(obj2)) {
+          System.out.println("Proved value equality .equals ");
+       } else {
+          System.out.println("Proved non-equality ");
+       }
+
+       /*
+        synchronized (obj1) {
+       ^
+  required: a type with identity
+       synchronized (obj1) {
+          System.out.println("Record object as monitor ");
+       }
+        */
+       // Values are never sclarized when passed to OSR buffer.
+       long res = 0;
+       for (int i = 0; i < 10000; i++) {
+           res += obj2.x() + obj2.y();
+       }
+       System.out.println("[obj2.x] " + obj2.x() + "  [obj2.y] " + obj2.y() + " [res]  " + res);
+   }
+}
